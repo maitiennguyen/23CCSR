8b8c9ecc625502be141e26ac97ab5a5dc1fd4705
maitiennguyen <117673140+maitiennguyen@users.noreply.github.com>
2024-02-23 10:23:28 -0700

    no longer parsing by set amount of spaces, using regrex to parse columns (more than two blank spaces is the delimiter)

--- convert_summary.py
+++ convert_summary.py
@@ -1,32 +1,17 @@
 import csv
 import sys
-
-column_widths = [50, 30, 100, 100]\
-
-headers = []
-lines = []
+import re
 
 def main(argv):
     
-    filename = argv[1]
+    input_file = argv[1]
+    output_file = 'converted_summary.csv'
 
-    with open(filename, 'r') as file:
-        for line_num, line in enumerate(file):
-            if line_num == 0:
-                headers.append(line[:column_widths[0]].strip())
-                headers.append(line[column_widths[1]:column_widths[1]+column_widths[0]+column_widths[1]].strip()[:10])
-                headers.append(line[column_widths[0]+column_widths[1]:].strip())
-            else:
-                row = []
-                row.append(line[:column_widths[0]].strip())
-                row.append(line[column_widths[1]:column_widths[1]+column_widths[0]+column_widths[1]].strip()[:1])
-                row.append(line[column_widths[0]+column_widths[1]:].strip())
-                lines.append(row)
-
-    with open('./converted_summary.csv', 'w', newline ='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerows([headers])
-        writer.writerows(lines)
+    with open(input_file, 'r') as in_file, open(output_file, 'w', newline='') as out_file:
+        writer = csv.writer(out_file)
+        for line in in_file:
+            row = re.split('\s{2,}', line.strip())
+            writer.writerow(row)
 
     
     
